7ec8a8834222707bfb4566eb2bed998128ef565e
Adarsh <iadarshh@gmail.com>
2021-12-12 17:30:30 +0530

    Timer

---
+++ Code/Lec9/4_firstTimer.js
@@ -0,0 +1,21 @@
+// node 4_firstTimer.js --n=10 --d=500 
+
+let minimist = require("minimist");
+let args = minimist(process.argv);
+
+let count = args.n;
+let time = args.d;
+
+console.log("Houston, Lets begin the countdown!");
+
+let id = setInterval(function(){
+            console.log("T minus " + count );
+            count--;
+            
+            if(count==0){
+                clearInterval(id);
+                console.log('>>>>>================================================>');
+            }
+},time);
+
+
