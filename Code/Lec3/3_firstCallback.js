526402b03da11093eae6a3b7ec89ece26e2576f3
Adarsh <m0400@Adarshs-MacBook-Pro.local>
2022-10-26 13:46:43 +0530

    Added console input

--- Code/Lec3/3_firstCallback.js
+++ Code/Lec3/3_firstCallback.js
@@ -6,7 +6,7 @@ let fs = require("fs");
 let args = minimist(process.argv);
 
 function IsPrime(x){
-    
+
     for(let div = 2; div < x; div++){
         if(x % div == 0){
             return false;
@@ -15,9 +15,9 @@ function IsPrime(x){
     return true;
 }
 
-// <---------------------------- TASK 1 BEGINS ----------------------------> 
+// <---------------------------- TASK 1 BEGINS ---------------------------->
 
-let t1 = Date.now();  // No.of milliseconds elapsed since 1 Jan 1970 
+let t1 = Date.now();  // No.of milliseconds elapsed since 1 Jan 1970
 console.log("Starting task1 at: "+t1 % 100000); // Getting the time, taking modulus to just show the last 5 on console.
 
 
@@ -25,17 +25,17 @@ console.log("Starting task1 at: "+t1 % 100000); // Getting the time, taking modu
 fs.readFile(args.source, function(data){    // While the file is being read from the disk, Task2 will start executing at the same time as it is the working of CPU.
 // Task2 need not wait for Task1 to be completed for its execution.
 
-    let t2 = Date.now(); 
-    console.log("Starting task1 at: "+t2 % 100000);
-    
+    let t2 = Date.now();
+    console.log("Ending task1 at: "+t2 % 100000);
+
     console.log(t2 - t1); // Estimated time to read the file
 
 }); // Reading the file asynchronously
 
-// <---------------------------- TASK 1 ENDS ----------------------------> 
+// <---------------------------- TASK 1 ENDS ---------------------------->
 
 
-// <---------------------------- TASK 2 BEGINS ----------------------------> 
+// <---------------------------- TASK 2 BEGINS ---------------------------->
 
 let t3 = Date.now();
 console.log("Starting task2 at: "+t3 % 100000);
@@ -45,13 +45,13 @@ for(let i = 2; i <= args.n ; i++){     // Check primes and push them in arr arra
     if(IsPrime(i)){
         arr.push(i);
     }
-} 
+}
 
 let t4 = Date.now();
-console.log("Starting task2 at: "+t4 % 100000);
+console.log("Ending task2 at: "+t4 % 100000);
 console.log(t4 - t3);
 
-// <---------------------------- TASK 2 ENDS ----------------------------> 
- 
+// <---------------------------- TASK 2 ENDS ---------------------------->
+
 // console.log((t2-t1) + (t4-t3));
-console.log(t4-t1); // Total Time taken by both tasks
+// console.log(t4-t1); // Total Time taken by both tasks
